3458bc270052ef835205aae2f003ea33b6ceb688
gixerr <pawel.wardyn@fenergo.com>
2021-08-23 19:43:06 +0200

    added logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,10 @@
+﻿import React from "react";
+import iisLogo from '../../assets/iis-logo.jpg'
+import classes from './Logo.module.css';
+const Logo = (props) => (
+    <div className={classes.logo}>
+        <img src={iisLogo} alt="iis Logo"/>
+    </div>
+);
+
+export default Logo;
