267ccd8ca476efdc76454ca0fa87b4517cd7a950
gixerr <pawel.wardyn@fenergo.com>
2021-08-23 19:29:49 +0200

    added axios configuration

---
+++ src/axios.js
@@ -0,0 +1,8 @@
+﻿import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'http://localhost:5001/'
+})
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+export default instance;
