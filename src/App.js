a2966f1eff735408f50ce3f51f7cf4a3237adf5c
gixerr <pawel.wardyn@fenergo.com>
2021-08-23 19:43:51 +0200

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Layout from './components/Layout/Layout';
+import { Route, Switch } from 'react-router-dom';
+import ApplicationPools from './containers/ApplicationPools/ApplicationPools';
+import Applications from './containers/Applications/Applications';
+import Builds from './containers/Builds/Builds';
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+        <Layout>
+            <Switch>
+                <Route path='/applicationPools' component={ApplicationPools}/>
+                <Route path='/applications' component={Applications}/>
+                <Route path='/builds' component={Builds}/>
+            </Switch>            
+        </Layout>
     </div>
   );
 }
