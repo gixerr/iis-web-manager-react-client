06553465b8d69932017d10f2a2e0fd8d091e5603
gixerr <pawel.wardyn@fenergo.com>
2021-08-23 19:42:56 +0200

    added layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+﻿import NavigationBar from '../Navigation/NavigationBar/NavigationBar';
+import classes from './Layout.module.css';
+
+const Layout = (props) => {
+    return (
+        <div className={classes.layout}>
+            <NavigationBar/>
+            <main>
+                {props.children}
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
