f0824f94a9917488e44e11a48f4d36048472db74
gixerr <pawel.wardyn@fenergo.com>
2021-08-23 19:34:48 +0200

    added applications container

---
+++ src/containers/Applications/Application/Application.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import classes from './Application.module.css'
+
+const Application = (props) => (
+    <div className={classes.application}>
+        <p>Name: <b>{props.name}</b></p>
+        <p>Application pool: <b>{props.applicationPoolName}</b></p>
+        <p>Application pool status: <b>{props.applicationPoolStatus}</b></p>
+        <p>Physical path: <b>{props.physicalPath}</b></p>
+    </div>
+);
+
+export default Application;
---
+++ src/containers/Applications/Applications.js
@@ -0,0 +1,27 @@
+﻿import React, { useEffect, useState } from 'react';
+import axios from '../../axios';
+import Application from './Application/Application';
+import classes from './Applications.module.css'
+
+const Applications = (props) => {
+    const [applications, setApplications] = useState([]);
+
+    useEffect(async () => {
+        axios.get('/sites/applications')
+            .then(response => (setApplications(response.data)));
+    });
+
+    return (
+        <section className={classes.applications__container}>
+            <header className={classes.container__header}>Applications</header>
+            {applications.map(application => <Application name={application.name}
+                                                          key={application.name}
+                                                          applicationPoolName={application.applicationPoolName}
+                                                          applicationPoolStatus={application.applicationPoolStatus}
+                                                          physicalPath={application.physicalPath}
+                                                />)}
+        </section>
+    )
+};
+
+export default Applications;
