909bbff86b52c8ca2c6ac024f1e6faa82f0ce59b
gixerr <pawel.wardyn@fenergo.com>
2021-08-29 20:21:46 +0200

    added Backdrop

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,8 @@
+﻿import React from "react";
+import classes from  './Backdrop.module.css'
+
+const backdrop = (props) => (
+    props.show ? <div className={classes.backdrop} onClick={props.clicked}/> : null
+);
+
+export default backdrop;
